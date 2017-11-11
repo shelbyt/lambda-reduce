00606450d0f2af1fca0ac4cb1fe1b3aa86f5e8ca
Vectorlee <vectorpku@gmail.com>
2017-11-10 19:00:32 -0800

    worker changed

--- worker.py
+++ worker.py
@@ -1,30 +1,28 @@
-import boto3
 
+import boto3
 S3_BUCKET = "shelby-lambda-in"
 
-
 def wordCount(data):
-	# assuming the data is a long string
-	wordList = data.split(' ')
-	wordDict = {}
-	for word in wordList:
-		if wordDict.has_key(word):
-			wordDict[word] += 1
-		else:
-			wordDict[word] = 1
-
-	return wordDict
+    # assuming the data is a long string
+    wordList = data.split(' ')
+    wordDict = {}
+    for word in wordList:
+        if wordDict.has_key(word):
+            wordDict[word] += 1
+        else:
+            wordDict[word] = 1
+    return wordDict
 
 
 def readData(filename, start, end):
 	if end < start:
-		raise Exception("Invalid input for data read")
+	    raise Exception("Invalid input for data read")
 
-    client = boto3.client("s3")
-    result = client.get_object(
-        Bucket = S3_BUCKET,
-        Key = filename,
-        Range = "bytes=" + start + "-" + end
+	client = boto3.client("s3")
+	result = client.get_object( \
+	    Bucket = S3_BUCKET, \
+	    Key = filename, \
+	    Range = "bytes=" + str(start) + "-" + str(end) \
     )
 
 	# read the entire body
@@ -36,24 +34,22 @@ def writeData(dataDict, index):
 
 	body = ""
 	for key in dataDict:
-		body += (key + "," + dataDict[key] + '\n')
+	    body += (key + "," + str(dataDict[key]) + '\n')
 
 	client = boto3.client("s3")
-	result = client.put_object(
-		Bucket = S3_BUCKET,
-		Key = tmpDataFile,
-		Body = body
+	result = client.put_object( \
+	    Bucket = S3_BUCKET, \
+	    Key = tmpDataFile, \
+	    Body = body \
 	)
 	return
 
+# read data from s3 bucket
+FileName = "dream.txt"
+data = readData(FileName, StartOffset, EndOffset)
 
-if __name__ == "__main__":
-	# read data from s3 bucket
-	FileName = "dream.txt"
-	data = readData(FileName, StartOffset, EndOffset)
+# do the calculation
+output = wordCount(data)
 
-	# do the calculation
-	output = wordCount(data)
-
-	# write the data back to s3 bucket
-	writeData(output, InstanceIndex)
+# write the data back to s3 bucket
+writeData(output, InstanceIndex)
